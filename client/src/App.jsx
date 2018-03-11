8b68e8ca3a862db5ba80b991ec19ef17d920c26f
Kevin Vo <kevinvoduy218@gmail.com>
2018-03-10 17:48:19 -0800

    [config] set up webpack

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        Hello react
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const path = require('path');
+require('dotenv').config();
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015', 'stage-0'],
+        }
+      }
+    ]
+  },
+  watch: true,
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'DEV_REST_SERVER_URL': JSON.stringify(process.env.DEV_REST_SERVER_URL),
+        'REST_SERVER_URL': JSON.stringify(process.env.REST_SERVER_URL),
+      }
+    }),
+  ]
+};
+
+//try to fix the path to get the react page to Rende
