c14da3daf62e284da026abcca0105d00b448648e
Kevin Vo <kevinvoduy218@gmail.com>
2018-03-11 14:59:14 -0700

    [jumbotron] made tweeks to style of jumbotron text and button

--- client/src/components/Jumbotron/Jumbotron.jsx
+++ client/src/components/Jumbotron/Jumbotron.jsx
@@ -5,11 +5,11 @@ export default class Jumobtron extends Component {
   render() {
     return (
       <div>
-        <img className='jumbo' src='https://www.hdwallpapers.in/walls/mountains_after_sunset_4k_8k-wide.jpg'/>
+        <img className='jumbo' src='https://www.hdwallpapers.in/walls/sunset_horizon_minimal_4k-HD.jpg'/>
 
         <div className='centered'>
-          <h2>Hello, I'm <strong>Kevin Vo</strong>.</h2>
-          <h2>I'm a full-stack web developer.</h2>
+          <p className='jumbo-text'>Hello, I'm <span className='myName'>Kevin Vo</span>.</p>
+          <p className='jumbo-text'>I'm a full-stack web developer.</p>
           <button className='button'>View my work</button>
         </div>
       </div>
