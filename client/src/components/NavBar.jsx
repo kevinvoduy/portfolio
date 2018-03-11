70e9e403a15304d67c2e5011896d96774b64ca1a
Kevin Vo <kevinvoduy218@gmail.com>
2018-03-11 13:33:30 -0700

    [add] semantic ui

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,46 +1,11 @@
 import React, { Component } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
 
 export default class NavBar extends Component {
-  state = { activeItem: 'home' }
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  state = {}
 
   render() {
-    const { activeItem } = this.state
-
     return (
-      <Segment inverted>
-        <Menu inverted pointing secondary>
-          <Menu.Item>
-            KK
-          </Menu.Item>
-
-          <Menu.Item
-            name='home'
-            active={activeItem === 'home'}
-            onClick={this.handleItemClick}
-          >
-            Home
-          </Menu.Item>
-
-          <Menu.Item
-            name='projects'
-            active={activeItem === 'projects'}
-            onClick={this.handleItemClick}
-          >
-            Projects
-          </Menu.Item>
-
-          <Menu.Item
-            name='connect'
-            active={activeItem === 'connect'}
-            onClick={this.handleItemClick}
-          >
-            Connect
-          </Menu.Item>
-        </Menu>
-      </Segment>
+      <div></div>
     )
   }
 }
