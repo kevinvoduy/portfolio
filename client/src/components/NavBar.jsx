0d9d5b4e588265d80e244043980fc3bca9950dff
Kevin Vo <kevinvoduy218@gmail.com>
2018-03-11 13:22:17 -0700

    [edit] change the style of navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,39 +1,46 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Segment } from 'semantic-ui-react'
 
 export default class NavBar extends Component {
-  state = {}
+  state = { activeItem: 'home' }
+
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
   render() {
     const { activeItem } = this.state
-    return (
-      <Menu stackable>
-        <Menu.Item>
-          <img/>
-        </Menu.Item>
 
-        <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
-          onClick={this.handleItemClick}
-        >
-          Home
-        </Menu.Item>
-        <Menu.Item
-          name='projects'
-          active={activeItem === 'projects'}
-          onClick={this.handleItemClick}
-        >
-          Projects
-        </Menu.Item>
-        <Menu.Item
-          name='connect'
-          active={activeItem === 'connect'}
-          onClick={this.handleItemClick}
-        >
-          Connect
-        </Menu.Item>
-      </Menu>
+    return (
+      <Segment inverted>
+        <Menu inverted pointing secondary>
+          <Menu.Item>
+            KK
+          </Menu.Item>
+
+          <Menu.Item
+            name='home'
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          >
+            Home
+          </Menu.Item>
+
+          <Menu.Item
+            name='projects'
+            active={activeItem === 'projects'}
+            onClick={this.handleItemClick}
+          >
+            Projects
+          </Menu.Item>
+
+          <Menu.Item
+            name='connect'
+            active={activeItem === 'connect'}
+            onClick={this.handleItemClick}
+          >
+            Connect
+          </Menu.Item>
+        </Menu>
+      </Segment>
     )
   }
 }
