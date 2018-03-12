628df57318c5ed5b0c0dd5091539d08bcd00314c
Kevin Vo <kevinvoduy218@gmail.com>
2018-03-11 17:00:08 -0700

    [add] about me section

---
+++ client/src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './aboutme.css'
+
+export default class AboutMe extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className='aboutme'>About Me</h1>
+      </div>
+    )
+  }
+}
