a2044b5c486b232b71f8ebfbf442ce82c045a285
Kevin Vo <kevinvoduy218@gmail.com>
2018-03-11 22:47:31 -0700

    [wip] building out email / outreach module

--- client/src/components/Connect/Connect.jsx
+++ client/src/components/Connect/Connect.jsx
@@ -2,11 +2,7 @@ import React, { Component } from 'react'
 import './connect.css'
 
 export default class Connect extends Component {
-  state = {
-    name,
-    email,
-    message,
-  };
+  state = {};
   onChangeHandler = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
@@ -20,26 +16,27 @@ export default class Connect extends Component {
     return (
       <div>
         <div className='heading'>
-          <h1>Contact</h1>}
+          <h1>Connect</h1>
           <h3>Have a question or want to work together?</h3>
         </div>
 
         <div className='form'>
           <form>
-            <label>
-              Name:
-              <input type='text' name='name' onChange={this.onChangeHandler} />
+            <label id='name'>
+              <input type='text' name='name' placeholder='Name' onChange={this.onChangeHandler} />
             </label>
+              <br/>
 
-            <label>
-              Email:
-              <input type='text' name='email' onChange={this.onChangeHandler} />
+            <label id='email'>
+              <input type='text' name='email' placeholder='Email' onChange={this.onChangeHandler} />
             </label>
+              <br/>
 
-            <label>
-              Message:
-              <input type='text' name='message' onChange={this.onChangeHandler} />
+            <label id='message'>
+              <input type='text' name='message' placeholder='Message' onChange={this.onChangeHandler} />
             </label>
+              <br/>
+
             <input type='submit' value='Submit'/>
           </form>
         </div>
