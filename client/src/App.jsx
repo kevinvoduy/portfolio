8897e24264ffa928147def72d71f2ea691e58062
Kevin Vo <kevinvoduy218@gmail.com>
2018-03-11 17:00:01 -0700

    [nav] is sticky when scrolled to top

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
-import NavBar from './components/NavBar/NavBar'
 import Jumbotron from './components/Jumbotron/Jumbotron'
+import NavBar from './components/NavBar/NavBar'
+import AboutMe from './components/AboutMe/AboutMe'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <NavBar/>
         <Jumbotron/>
+        <NavBar/>
+          <br/>
+        <AboutMe/>
+          <br/>
+          <h2 className='stupidP'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h2>
+          <h2 className='stupidP'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h2>
+          <h2 className='stupidP'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h2>
+          <h2 className='stupidP'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h2>
+          <h2 className='stupidP'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h2>
+          <h2 className='stupidP'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h2>
+          <h2 className='stupidP'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h2>
+          <h2 className='stupidP'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h2>
+
       </div>
     )
   }
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,19 +2,40 @@ import React, { Component } from 'react'
 import './navbar.css'
 
 export default class NavBar extends Component {
-  state = {}
+  state = {
+    topOfNav: 0,
+    paddingOffset: 0,
+  }
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.handleScroll);
+
+    const nav = document.querySelector('.nav');
+    this.setState({
+      topOfNav: nav.offsetTop,
+      paddingOffset: nav.offsetHeight,
+    });
+  }
+
+  handleScroll = () => {
+    if (window.scrollY >= this.state.topOfNav) {
+      document.body.style.paddingTop = this.state.paddingOffset + 'px';
+      document.body.classList.add('fixed-nav');
+    } else {
+      document.body.style.paddingTop = 0;
+      document.body.classList.remove('fixed-nav');
+    }
+  }
+
   render() {
     return (
-      <div className="ui stackable menu">
-        <div className="item">
-          <img/>
-        </div>
-        <a className="item" name='Home'>Home</a>
-
-        <a className="item" name='Projects'>Projects</a>
-
-        <a className="item" name='Connect'>Connect</a>
+      <div className="nav">
+        <ul>
+          <li>LOGO.</li>
+          <li>home</li>
+          <li>projects</li>
+          <li>connect</li>
+        </ul>
       </div>
-    )
+    );
   }
 }
