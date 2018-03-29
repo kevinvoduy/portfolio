ff1caa219efbe765342add6a3af4c10533c92717
Kevin V <kevinvoduy218@gmail.com>
2018-03-28 23:13:26 -0700

    [port] changed dev port number

--- rest-server/src/index.js
+++ rest-server/src/index.js
@@ -4,7 +4,7 @@ import App from './config/express';
 const app = App.express;
 
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, (err) => {
   if (err) throw new Error;
