198e55bc42b5234a6dc13a0fa31117b354637da6
Kevin V <kevinvoduy218@gmail.com>
2018-03-28 23:30:09 -0700

    [projects] added first project image

--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -1,39 +1,40 @@
 import React, { Component } from 'react';
-import Project from './Project';
+// import Project from './Project';
 import slope from './slope.svg';
 import './projects.css';
+import project1 from '../../../public/dist/gowithme.png';
 
 export default class Projects extends Component {
   componentDidMount() {
-    const slider = document.querySelector('.projects');
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+    // const slider = document.querySelector('.projects');
+    // let isDown = false;
+    // let startX;
+    // let scrollLeft;
 
-    slider.addEventListener('mousedown', (e) => {
-      isDown = true;
-      slider.classList.add('active');
-      startX = e.pageX - slider.offsetLeft;
-      scrollLeft = slider.scrollLeft;
-    });
+    // slider.addEventListener('mousedown', (e) => {
+    //   isDown = true;
+    //   slider.classList.add('active');
+    //   startX = e.pageX - slider.offsetLeft;
+    //   scrollLeft = slider.scrollLeft;
+    // });
 
-    slider.addEventListener('mouseleave', () => {
-      isDown = false;
-      slider.classList.add('active');
-    });
+    // slider.addEventListener('mouseleave', () => {
+    //   isDown = false;
+    //   slider.classList.add('active');
+    // });
 
-    slider.addEventListener('mouseup', () => {
-      isDown = false;
-      slider.classList.add('active');
-    });
+    // slider.addEventListener('mouseup', () => {
+    //   isDown = false;
+    //   slider.classList.add('active');
+    // });
 
-    slider.addEventListener('mousemove', (e) => {
-      if (!isDown) return;
-      e.preventDefault();
-      const x = e.pageX - slider.offsetLeft;
-      const walk = x - startX;
-      slider.scrollLeft = scrollLeft - walk;
-    });
+    // slider.addEventListener('mousemove', (e) => {
+    //   if (!isDown) return;
+    //   e.preventDefault();
+    //   const x = e.pageX - slider.offsetLeft;
+    //   const walk = x - startX;
+    //   slider.scrollLeft = scrollLeft - walk;
+    // });
   }
 
   render() {
@@ -41,35 +42,44 @@ export default class Projects extends Component {
     return (
       <div id="linkToProjects">
         <h2 id="projects">Recent Projects.</h2>
-        <div className="projects" id="scroll">
-          <Project
-            title="Levy Tran"
-            meta="Ex Girlfriend"
-            description="Jaes new girlfriend"
-          />
-          <Project
-            title="Levy Tran"
-            meta="Ex Girlfriend"
-            description="Jaes new girlfriend"
-          />
-          <Project
-            title="Levy Tran"
-            meta="Ex Girlfriend"
-            description="Jaes new girlfriend"
-          />
-          <Project
-            title="Levy Tran"
-            meta="Ex Girlfriend"
-            description="Jaes new girlfriend"
-          />
-          <Project
-            title="Levy Tran"
-            meta="Ex Girlfriend"
-            description="Jaes new girlfriend"
-          />
+        <div className="row">
+          <div id="project1">
+            <img src={project1} alt="Go With Me App" />
+          </div>
         </div>
+
+
         <div className="slope"><img alt="" src={slope} /></div>
       </div>
     );
   }
 }
+
+// SEMANTIC PROJECT TEMPLATE
+// <div className="projects" id="scroll">
+//   <Project
+//     title="Levy Tran"
+//     meta="Ex Girlfriend"
+//     description="Jaes new girlfriend"
+//   />
+//   <Project
+//     title="Levy Tran"
+//     meta="Ex Girlfriend"
+//     description="Jaes new girlfriend"
+//   />
+//   <Project
+//     title="Levy Tran"
+//     meta="Ex Girlfriend"
+//     description="Jaes new girlfriend"
+//   />
+//   <Project
+//     title="Levy Tran"
+//     meta="Ex Girlfriend"
+//     description="Jaes new girlfriend"
+//   />
+//   <Project
+//     title="Levy Tran"
+//     meta="Ex Girlfriend"
+//     description="Jaes new girlfriend"
+//   />
+// </div>
--- rest-server/src/index.js
+++ rest-server/src/index.js
@@ -4,7 +4,7 @@ import App from './config/express';
 const app = App.express;
 
 const server = http.createServer(app);
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, (err) => {
   if (err) throw new Error;
