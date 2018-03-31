879123da3099662f3530cb95e8f488bdf0807af9
Kevin V <kevinvoduy218@gmail.com>
2018-03-30 20:58:22 -0700

    [jumbotron] new text feature

--- client/src/components/Jumbotron/Jumbotron.jsx
+++ client/src/components/Jumbotron/Jumbotron.jsx
@@ -8,12 +8,13 @@ const Jumbotron = () => (
 
     <div className="centered">
       <p className="jumbo-text">{'Hello, I\'m '}<span className="myName">Kevin Vo</span>.</p>
-      <p className="jumbo-text">{'I\'m a full stack web developer.'}</p>
+      <div className="typewriter jumbo-text">
+        <p>{'I\'m a full stack web developer.'}</p>
+      </div>
+
       <a href="#projects"><button className="button"><span>View my work</span></button></a>
     </div>
   </div>
 );
 
 export default Jumbotron;
-
-// https://www.hdwallpapers.in/walls/sunset_horizon_minimal_4k-HD.jpg
