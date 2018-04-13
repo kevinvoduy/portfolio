47b9f105e7562096d9f9298bc3dc3aaed4420b83
Kevin V <kevinvoduy218@gmail.com>
2018-04-13 14:27:37 -0700

    [jumbotron] edits for mobile

--- client/src/components/Jumbotron/Jumbotron.jsx
+++ client/src/components/Jumbotron/Jumbotron.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import desk from '../../../public/dist/desk.jpg';
-import oceanalt from '../../../public/dist/ocean-alt.jpg';
 import './jumbotron.css';
 
 const Jumbotron = () => (
   <div className="container">
     <img className="jumbo" src={desk} alt="" />
-    <img className="jumbo-alt" alt="" />
 
     <div className="centered">
-      <p className="jumbo-text">{'Hello, I\'m '}<span className="myName">Kevin Vo</span>.</p>
-      <div className="typewriter jumbo-text">
-        <p>{'I\'m a full stack engineer.'}</p>
+      <div className="sad">
+        <p className="jumbo-text">{'Hello, I\'m '}<span className="myName">Kevin Vo</span>.</p>
+        <div className="typewriter jumbo-text">
+          <p>{'I\'m a full stack engineer.'}</p>
+        </div>
       </div>
 
       <a href="#projects"><button className="button"><span>View my work</span></button></a>
