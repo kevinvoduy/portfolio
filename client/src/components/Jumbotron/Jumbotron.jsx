e7a04a00ebbdb85db11659db49daaa2baba08b53
Kevin V <kevinvoduy218@gmail.com>
2018-07-07 16:08:53 -0700

    [jumbotron] added type module

--- client/src/components/Jumbotron/Jumbotron.jsx
+++ client/src/components/Jumbotron/Jumbotron.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
+import Typed from 'typed.js';
 import './jumbotron.css';
 
-const Jumbotron = () => (
-  <div className="container" id="container">
-    <div className="jumbo" />
-    <div className="centered">
-      <div className="sad">
-        <p className="jumbo-text">{'Hello, I\'m '}<span className="myName">Kevin Vo</span>.</p>
-        <div className="typewriter jumbo-text">
-          <p>{'I\'m a full stack engineer.'}</p>
+class Jumbotron extends React.Component {
+  componentDidMount() {
+    const strings = ['a full stack engineer.', 'also unemployed.'];
+    const options = {
+      strings,
+      typeSpeed: 80,
+      backSpeed: 50,
+      loop: true,
+      backDelay: 2500,
+    };
+
+    this.typed = new Typed(this.el, options);
+  }
+
+  render() {
+    return (
+      <div className="container" id="container">
+        <div className="jumbo" />
+        <div className="centered">
+          <div className="sad">
+            <p className="jumbo-text">{'Hello, I\'m '}<span className="myName">Kevin Vo</span>{'. I am'}</p>
+            <div className="typewriter jumbo-text">
+              <span id="typed-js" ref={(el) => { this.el = el; }} />
+            </div>
+          </div>
+
+          <a href="#projects"><button className="button"><span>View my work</span></button></a>
         </div>
       </div>
+    );
+  }
+}
 
-      <a href="#projects"><button className="button"><span>View my work</span></button></a>
-    </div>
-  </div>
-);
 export default Jumbotron;
